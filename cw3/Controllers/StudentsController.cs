3b2a56246e8edd2153347c7e4f0168bcb3af6e9a
s16796 <s16796@pjwstk.edu.pl>
2020-03-29 17:39:08 +0200

    First full app commit

---
+++ cw3/Controllers/StudentsController.cs
@@ -0,0 +1,69 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using cw3.DAL;
+using cw3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+
+    public class StudentsController : ControllerBase
+    {
+
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbservice)
+        {
+            _dbService = dbservice;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            
+            int numberofstudents = _dbService.getStudents().Count();
+            IEnumerator<Student> enumerator = _dbService.getStudents().GetEnumerator();
+            for (int i = 0; i <= numberofstudents; i++)
+            {
+                if (enumerator.Current != null && enumerator.Current.IdStudent == id)
+                {
+                    return Ok(enumerator.Current);
+                }
+
+                enumerator.MoveNext();
+            }
+            enumerator.Dispose();
+            return Ok("Student not found");
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.getStudents());
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.indexNumber = $"s{new Random().Next(1, 99999)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id, Student student)
+        {
+            return Ok("Aktualizacja studenta nr " + id + " dokończona.");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("Usuwanie studenta nr " +id+ " ukończone.");
+        }
+
+    }
+}
---
+++ cw3/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿﻿namespace cw3.Models
+{
+    public class Student
+    {
+        public int IdStudent { get; set; }
+        
+        public string firstName { get; set; }
+        
+        public string lastName { get; set; }
+        
+        public string indexNumber { get; set; }
+    }
+}
